76286b0874bef6dcdfdfdb63cd000639e7df14b9
code-with-ricky <amrikdevofficial@gmail.com>
2026-05-31 16:47:30 +0530

    first program hello world done

---
+++ Chapter_1_CPP_Basics/P1_HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include<iostream>  // header file library for input and output objects such as cout
+using namespace std; // we can use names for objects and variables from the standard library
+
+int main() {
+    // cout is an object used together with insertion operator (<<) to output/print text
+    cout << "Hello World!";
+    return 0;
+}
